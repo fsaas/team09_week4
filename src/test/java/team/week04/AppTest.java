02a6740e3163202c2e96065f34d9300a8ab4464e
Se-mok, Choi <csm950907@naver.com>
2016-04-14 12:21:11 +0900

    test code fix

--- src/test/java/team/week04/AppTest.java
+++ src/test/java/team/week04/AppTest.java
@@ -40,5 +40,7 @@ public class AppTest
         assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
         tcalc.setUser(new User(300, 3, "Gold"));
         assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Gold")));
+        tcalc.setUser(new User(4000, 3, "Silver"));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(4000, 3, "Silver")));
     }
 }
