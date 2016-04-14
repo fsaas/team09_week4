5131c9fafab49fcf6d24c88d13c754d498cdea0d
Woojin Jo <Woojin.jo95@gmail.com>
2016-04-14 22:56:24 +0900

    복붙한듯한 코드 수정함

--- src/test/java/team/week04/AppTest.java
+++ src/test/java/team/week04/AppTest.java
@@ -37,32 +37,32 @@ public class AppTest
         Output output = new Output();
         
         tcalc.setUser(new User(30, 2, "Silver"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(30, 2, "Silver")));
         
 
         tcalc.setUser(new User(30, 2, "Gold"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(30, 2, "Silver")));
 
         tcalc.setUser(new User(30, 6, "Silver"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(30, 6, "Silver")));
         
 
         tcalc.setUser(new User(30, 6, "Gold"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(30, 6, "Silver")));
 
 
         tcalc.setUser(new User(1500, 3, "Silver"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(1500, 3, "Silver")));
 
         tcalc.setUser(new User(1500, 3, "Gold"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(1500, 3, "Silver")));
         
 
         tcalc.setUser(new User(1500, 6, "Silver"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(1500, 3, "Silver")));
 
         tcalc.setUser(new User(1500, 6, "Gold"));
-        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Silver")));
+        assertEquals(tcalc.calcCharge(), output.calcCharge(new User(1500, 3, "Silver")));
 
         tcalc.setUser(new User(300, 3, "Gold"));
         assertEquals(tcalc.calcCharge(), output.calcCharge(new User(300, 3, "Gold")));
