24e274b242018f6cf76e87467c5e4df656f18f4d
Se-mok, Choi <csm950907@naver.com>
2016-04-14 12:31:57 +0900

    code fix

--- src/main/java/team/week04/Output.java
+++ src/main/java/team/week04/Output.java
@@ -1,11 +1,7 @@
 package team.week04;
 
-import java.util.Scanner;
-
 public class Output {
 	//private variables
-	private Scanner scanner;
-	private User user;
 	private Calc calculator;
 
 	//constructor
@@ -14,26 +10,6 @@ public class Output {
 	}
 	
 	//public methods
-	public void outputBill()
-	{		
-		float givenUsed;
-		int givenline;
-		String givenplan;
-		
-		givenUsed = scanner.nextFloat();
-		givenline = scanner.nextInt();
-		givenplan = scanner.next();
-		
-		user = new User(givenUsed, givenline, givenplan);
-		
-		this.calculator.setUser(this.user);
-		System.out.println("이번 달의 사용 결과는 아래와 같습니다.");
-		System.out.println("총 사용량 : " + user.getUsedRate());
-		System.out.println("사용 회선 수 : " + user.getLine());
-		System.out.println("사용중인 플랜 : " + user.getRank());
-		System.out.println("납부하셔야 하는 요금 : " + this.calcCharge(user) );
-	}
-	
 	public float calcCharge(User user){
 		this.calculator.setUser(user);
 		return this.calculator.calcCharge();
